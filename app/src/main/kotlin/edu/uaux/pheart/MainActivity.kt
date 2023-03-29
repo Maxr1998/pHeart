4b364e096f4a06135219a80d6d76d8dd2c4ed664
Maxr1998 <max.rumpf1998@gmail.com>
2023-03-29 10:14:39 +0200

    :art: Optimize imports

--- app/src/main/kotlin/edu/uaux/pheart/MainActivity.kt
+++ app/src/main/kotlin/edu/uaux/pheart/MainActivity.kt
@@ -2,8 +2,6 @@ package edu.uaux.pheart
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
-import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
