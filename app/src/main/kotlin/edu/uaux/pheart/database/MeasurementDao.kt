8a1c04177d62084b7d0749d7728cadf8ce60de3a
TFackler <38538728+TFackler@users.noreply.github.com>
2023-04-12 13:46:49 +0200

    :recycle: Rename room database table

--- app/src/main/kotlin/edu/uaux/pheart/database/MeasurementDao.kt
+++ app/src/main/kotlin/edu/uaux/pheart/database/MeasurementDao.kt
@@ -10,13 +10,13 @@ import java.time.ZonedDateTime
 @Dao
 interface MeasurementDao {
 
-    @Query("select * from user order by timestamp desc")
+    @Query("select * from measurement order by timestamp desc")
     fun getAll(): LiveData<List<Measurement>>
 
     /**
      * Note: start and end inclusive
      */
-    @Query("select * from user where timestamp between (:start) and (:end) order by timestamp desc")
+    @Query("select * from measurement where timestamp between (:start) and (:end) order by timestamp desc")
     suspend fun getAll(start: ZonedDateTime, end: ZonedDateTime): List<Measurement>
 
     @Insert
@@ -28,6 +28,6 @@ interface MeasurementDao {
     @Delete
     fun delete(measurement: Measurement)
 
-    @Query("delete from user")
+    @Query("delete from measurement")
     fun deleteAll()
 }
